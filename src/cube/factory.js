3f6d255399c0703d3e9e011c1e1ed25ae1dd738f
Derek Nelson <dereknelson01@gmail.com>
2017-06-02 16:04:37 -0400

    Add Cube Factory

---
+++ src/cube/factory.js
@@ -0,0 +1,14 @@
+module.exports = class CubeFactory {
+
+    constructor() {
+        this.sides = [];
+    }
+
+    addSide (array) {
+        this.sides.push(array);
+    }
+
+    getCube () {
+
+    }
+};
