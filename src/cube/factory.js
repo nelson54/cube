d27163e422f2883e673fc4c223a7bb1acbf92a4b
Austin Dumm <adumm@cambiumlearning.com>
2017-06-02 16:34:19 -0400

    Shows adding side and then getCube creates a cube from those sides

--- src/cube/factory.js
+++ src/cube/factory.js
@@ -5,10 +5,16 @@ module.exports = class CubeFactory {
     }
 
     addSide (array) {
+        //array is a 3x3 of a side
         this.sides.push(array);
     }
 
     getCube () {
+        var colors = [];
+        for(var side = 0; side < this.sides.length; ++side) {
+            colors.push(this.sides[side][1][1])
+        }
 
+        return new Cube(colors, this.sides);
     }
 };
